65f48ca449ce677e346c576348ad6f0c36a95457
Chiedozie Michael <michaelcee2000@gmail.com>
2026-06-11 13:15:00 +0100

    fix: deploy settings

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/portfolio-michael-chiedozie/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
