9a848015e2fc472590f325b6a287046470be9009
Chiedozie Michael <michaelcee2000@gmail.com>
2026-06-20 07:00:47 +0100

    style: format import statement for Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero           from './components/Hero/Hero';
 import About          from './components/About/About';
 import Skills         from './components/Skills/Skills';
 import Work           from './components/Work/Work';
-import Contact        from './components/Contact/Contact';
+import Contact from './components/Contact/Contact';
 import Footer         from './components/Footer/Footer';
 import ScrollProgress from './components/ScrollProgress/ScrollProgress';
 import ScrollToTop    from './components/ScrollToTop/ScrollToTop';
